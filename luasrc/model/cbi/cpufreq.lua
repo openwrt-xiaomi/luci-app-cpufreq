7910f1ff8d964d8bc168d9559034adbbac5b3729
remittor <remittor@gmail.com>
2022-08-21 16:16:26 +0300

    Adapt for kernel 5.10

--- luasrc/model/cbi/cpufreq.lua
+++ luasrc/model/cbi/cpufreq.lua
@@ -1,11 +1,14 @@
 local fs = require "nixio.fs"
 
-function string.split(input, delimiter)
+function string_split(input, delimiter)
 	input = tostring(input)
 	delimiter = tostring(delimiter)
-	if (delimiter=='') then return false end
-	local pos,arr = 0, {}
-	for st,sp in function() return string.find(input, delimiter, pos, true) end do
+	if delimiter == '' then
+		return false
+	end
+	local pos = 0
+	local arr = {}
+	for st, sp in function() return string.find(input, delimiter, pos, true) end do
 		table.insert(arr, string.sub(input, pos, st - 1))
 		pos = sp + 1
 	end
@@ -20,18 +23,35 @@ s = mp:section(NamedSection, "cpufreq", "settings")
 s.anonymouse = true
 
 local policy_nums = luci.sys.exec("echo -n $(find /sys/devices/system/cpu/cpufreq/policy* -maxdepth 0 | grep -Eo '[0-9]+')")
-for _, policy_num in ipairs(string.split(policy_nums, " ")) do
-	if not fs.access("/sys/devices/system/cpu/cpufreq/policy" .. policy_num .. "/scaling_available_frequencies") then return end
 
-	cpu_freqs = fs.readfile("/sys/devices/system/cpu/cpufreq/policy" .. policy_num .. "/scaling_available_frequencies")
-	cpu_freqs = string.sub(cpu_freqs, 1, -3)
+for _, policy_num in ipairs(string_split(policy_nums, " ")) do
+	local cpu_drv_type = 0
+	local cpu_freqs = nil
+	local freq_array = {}
+	local cpu_dir = "/sys/devices/system/cpu/cpufreq"
+	local cpufreq_list1 = cpu_dir .. "/policy" .. policy_num .. "/scaling_available_frequencies"
+	local cpufreq_list2 = cpu_dir .. "/policy" .. policy_num .. "/stats/trans_table"
+	if fs.access(cpufreq_list1) then
+		cpu_drv_type = 1
+		cpu_freqs = fs.readfile(cpufreq_list1)
+		cpu_freqs = string.sub(cpu_freqs, 1, -3)
+		freq_array = string_split(cpu_freqs, " ")
+	elseif fs.access(cpufreq_list2) then
+		cpu_drv_type = 2
+		cpu_freqs = fs.readfile(cpufreq_list2)
+		for v in string.gmatch(cpu_freqs, "[^\n][%s]*(%d+)[%s]*:") do
+			table.insert(freq_array, v)
+		end
+	end
+	if not freq_array or not cpu_freqs or cpu_drv_type == 0 then
+		nixio.syslog("info", "[CpuFreq] Current CPU driver not supported!")
+		return
+	end
 
 	cpu_governors = fs.readfile("/sys/devices/system/cpu/cpufreq/policy" .. policy_num .. "/scaling_available_governors")
 	cpu_governors = string.sub(cpu_governors, 1, -3)
 
-
-	freq_array = string.split(cpu_freqs, " ")
-	governor_array = string.split(cpu_governors, " ")
+	governor_array = string_split(cpu_governors, " ")
 
 	s:tab(policy_num, translate("Policy " .. policy_num))
 
